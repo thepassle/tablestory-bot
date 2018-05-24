74a9570e8f22813ce761896e513d8db5b390a600
Kujio <astroc@gmail.com>
2018-05-24 11:57:48 -0400

    Implemented config file and parsing. Updated for python 3 Numerous bug fixes

--- tablestorybot.py
+++ tablestorybot.py
@@ -1,12 +1,15 @@
 import time
+import ast
 import socket
 import random
 import re
 import urllib, json
-import MySQLdb
+import pymysql
 import string
 import requests
 import datetime
+import configparser
+
 from threading import Thread
 
 # needs a timer (and be able toadd a command to the timer)
@@ -15,12 +18,33 @@ from threading import Thread
 # dice rolling? !roll 1d20
 # make the code better pnySmile
 
-HOST = "irc.twitch.tv"
-PORT = 6667
+# Load config.ini
+config = configparser.ConfigParser()
+config.read('config.ini')
+print(config.sections())
 
-PASS = "oauth:XXXXXXXXXXXXXXXXXXXXX"
-IDENT = "tablestorybot"
-CHANNEL = "tablestory"
+def dbGetOne(query):
+    db = pymysql.connect(config["Database"]["HOSTNAME"],config["Database"]["USERNAME"],config["Database"]["PASSWORD"],config["Database"]["DBNAME"] )
+    cursor = db.cursor()
+    cursor.execute(query)
+    data = cursor.fetchone()
+    db.close()
+    return data
+
+def dbGetAll(query):
+    db = pymysql.connect(config["Database"]["HOSTNAME"],config["Database"]["USERNAME"],config["Database"]["PASSWORD"],config["Database"]["DBNAME"] )
+    cursor = db.cursor()
+    cursor.execute(query)
+    data = cursor.fetchall()
+    db.close()
+    return data
+
+def dbExecute(query):
+    #db = pymysql.connect("www52.totaalholding.nl","thepas1q_tablestorybot","meme6Sux","thepas1q_tablestory" )
+    db = pymysql.connect(config["Database"]["HOSTNAME"],config["Database"]["USERNAME"],config["Database"]["PASSWORD"],config["Database"]["DBNAME"] )
+    cursor = db.cursor()
+    cursor.execute(query)
+    db.close()
 
 def getUser(line):
     separate = line.split(":", 2)
@@ -34,27 +58,28 @@ def getMessage(line):
 
 def openSocket():
     s = socket.socket()
-    s.connect((HOST, PORT))
-    s.send("PASS " + PASS + "\r\n")
-    s.send("NICK " + IDENT + "\r\n")
-    s.send("JOIN #" + CHANNEL + "\r\n")
+    s.connect((config["Twitch"]["HOST"], int(config["Twitch"]["PORT"])))
+    s.send(str("PASS " + config["Twitch"]["PASS"] + "\r\n").encode("utf-8"))
+    s.send(str("NICK " + config["Twitch"]["IDENT"] + "\r\n").encode("utf-8"))
+    s.send(str("JOIN #" + config["Twitch"]["CHANNEL"] + "\r\n").encode("utf-8"))
     return s
     
 def sendMessage(s, message):
-    messageTemp = "PRIVMSG #" + CHANNEL + " :" + message
-    s.send(messageTemp + "\r\n")
+    messageTemp = "PRIVMSG #" + config["Twitch"]["CHANNEL"]+ " :" + message
+    s.send(str(messageTemp + "\r\n").encode("utf-8"))
     print("Sent: " + messageTemp)
 
 def joinRoom(s):
     readbuffer = ""
     Loading = True
     while Loading:
-        readbuffer = readbuffer + s.recv(1024)
-        temp = string.split(readbuffer, "\n")
+        readbuffer = readbuffer + s.recv(1024).decode("utf-8")
+        temp = str.split(readbuffer, "\n")
         readbuffer = temp.pop()
         for line in temp:
             print(line)
             Loading = loadingComplete(line)
+    print("Finished Connecting...")
     
 def loadingComplete(line):
     if("End of /NAMES list" in line):
@@ -63,8 +88,7 @@ def loadingComplete(line):
         return True
 
 def is_live_stream(streamer_name, client_id):
-    twitch_api_stream_url = "https://api.twitch.tv/kraken/streams/" + streamer_name + "?client_id=" + client_id
-
+    twitch_api_stream_url = "https://api.twitch.tv/kraken/streams/" + streamer_name + "?client_id=" + config["Twitch"]["CLIENT_ID"]
     streamer_html = requests.get(twitch_api_stream_url)
     streamer = json.loads(streamer_html.content)
 
@@ -80,10 +104,12 @@ def is_live_stream(streamer_name, client_id):
 # loopThread.start()
 
 def getModList():
+    print("Getting modlist")
     url = "https://tmi.twitch.tv/group/user/tablestory/chatters"
-    response = urllib.urlopen(url)
-    data = json.loads(response.read())
+    response = urllib.request.urlopen(url)
+    data = json.loads(response.read().decode("utf-8"))
     moderators = []
+    
     for name in data["chatters"]["moderators"]:
         moderators.append(name)
     return moderators
@@ -92,38 +118,45 @@ s = openSocket()
 joinRoom(s)
 readbuffer = ""
 message = ""
+mods = getModList()
 
 while True:
     while True:
         try:
 
-            mods = getModList()
-            mods.append('karlklaxon')
+            
 
             try:
                 chat_data =  s.recv(1024)
+                
             except socket.timeout:
-                print "Err"
+                print("Error: disconnected.. Reconnecting")
+                s = openSocket()
+                joinRoom(s)
+                continue;
 
-            readbuffer = readbuffer + chat_data
-            temp = readbuffer.split('\n')
+            readbuffer = readbuffer + chat_data.decode("utf-8")
+            temp = readbuffer.split('\r\n')
             readbuffer = temp.pop()
             if readbuffer == "":
                 pass
-
+            
             for line in temp: 
                 if "PING" in line:
-                    s.send("PONG :tmi.twitch.tv\r\n".encode())
-
+                    s.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
+                    continue
                 user = getUser(line)
+                mods = getModList()
+                if user in mods:
+                    print("User is moderator.")
                 message = getMessage(line)
 
-                print "{} typed: {} \n".format(user, message)
+                print("{} typed: {} \n".format(user, message))
 
 
                 if re.search(r"[a-zA-Z]{2,}\.[a-zA-Z]{2,}", message ) and user not in mods:
-                    sendMessage(s, "/timeout "+user+" 1")
-                    sendMessage(s, "Links are not allowed!")
+                    sendMessage(s, "/timeout "+user+" 1".encode("utf-8"))
+                    sendMessage(s, "Links are not allowed!".encode("utf-8"))
 
 #####################################################################################################################
                                                     ## COMMANDS ## 
@@ -132,29 +165,39 @@ while True:
                 # this REALLY needs to be changed
                 if message[0] == "!":
                     allCommands = dbGetAll("SELECT * FROM commands")
-
+                    #print(allCommands)
                     for command in allCommands:
                         trigger = str(command[0])
-                        reply = str(command[1])
-                        clearance = str(command[2])
+                        #print(command[1])
+                        reply = str(command[1][2:-1])
+                        replyb = bytes(reply, "utf-8")
+                        
+                        replye = ast.literal_eval(str(replyb).replace('\\\\', '\\'))
+                        replyf = replye.decode("utf-8")
 
-                        if re.search(r""+trigger+" [a-zA-Z0-9]+", message ):
+                        
+                        
+                        clearance = str(command[2])
+                        
+                        
+                        
+                        if re.search(r""+trigger+" [@]?[a-zA-Z0-9]+", message ):
                             if clearance == 'mod' and user not in mods:
                                 pass
                             else:
                                 target = message.strip().split(' ',1)[1] 
-                                sendMessage(s, "@" + target + ": " + reply)
-
+                                print("this should @ target and print message.")
+                                sendMessage(s, target +": " + replyf)
                         elif message == trigger:
                             if clearance == 'mod' and user not in mods:
                                 pass
                             else:
-                                sendMessage(s, reply)
+                                sendMessage(s, replyf)
 
 
                 #add command
-                if re.search(r"!addcom -ul=all ![a-zA-Z0-9]+", message ) or re.search(r"!addcommand -ul=mod ![a-zA-Z0-9]+", message ) and user in mods:
-                    print "** Adding command **"
+                if re.search(r"!addcom -ul=all ![a-zA-Z0-9]+", message ) or re.search(r"!addcom -ul=mod ![a-zA-Z0-9]+", message ) and user in mods:
+                    print("** Adding command **")
                     #if theres only '!addcom' and '!someword', but no reply
                     if len(message.split(' ')) <= 3:
                         pass
@@ -165,65 +208,74 @@ while True:
                         clearance = str(message[1].split('=')[1])
                         command = str(message[2])
                         reply = str(message[3])
-                        if command[0] == '!':
-                            dbExecute("INSERT INTO commands (command, reply, clearance) VALUES ( '"+command+"', '"+reply+"', '"+clearance+"' )" )
-                            sendMessage(s, "Command: '"+command+"' added.")
+                        print(reply.encode("utf-8"))
 
+                        if command[0] == '!':
+                            query = "INSERT INTO commands (command, reply, clearance) VALUES ( '"+command+"', \""+ str(reply.encode("utf-8")) +"\" , '"+clearance+"' )"
+                            query = query.replace('\\', '\\\\' )
+
+                            dbExecute(query)
+                            sendMessage(s, "Command: '"+command+"' added.")
+                            continue
 
                 if re.search(r"!delcom ![a-zA-Z0-9]+", message ) and user in mods:
-                    print "** Removing command **"
+                    print("** Removing command **")
                     message = message.split(' ', 2)
 
                     dbExecute("DELETE FROM commands WHERE command='"+str(message[1]).strip()+"' ")
                     sendMessage(s, "Command: '"+str(message[1])+"' deleted.")
-
+                    continue
 #####################################################################################################################
                                                     ## UTILS ## 
 #####################################################################################################################
 
-                if re.search(r"!caster [a-zA-Z0-9_]+", message ) and user in mods:
-                    print "** Caster command **"
+                if re.search(r"^!caster [a-zA-Z0-9_]+", message ) and user in mods:
+                    print("** Caster command **")
 
                     message = message.split(' ')
+                    
 
-                    twitch_api_stream_url = "https://api.twitch.tv/kraken/channels/"+message[1]+"?client_id=7n93eyua2byab5b1izk7opodhioahd"
-                    streamer_html = requests.get(twitch_api_stream_url)
-                    streamer = json.loads(streamer_html.content)
+                    twitch_api_stream_url = "https://api.twitch.tv/kraken/channels/"+message[1]+"?client_id=" + config["Twitch"]["CLIENT_ID"]
+                    streamer_html = urllib.request.urlopen(twitch_api_stream_url)
+                    
+
+                    streamer = json.loads(streamer_html.read().decode("utf-8"))
+                    
                     game = streamer["game"]
 
                     sendMessage(s, "We love @"+message[1]+", go give them a follow at www.twitch.tv/"+message[1]+" ! They were last seen playing "+str(game))
-
+                    continue
 #####################################################################################################################
                                                     ## QUOTES ## 
 #####################################################################################################################
 
                 if "!quote" in message or "!addquote" in message or "!delquote" in message:
                     dataquotes = dbGetAll("SELECT * FROM quotes")
-                    print dataquotes
+                    print(dataquotes)
                     totalquotes = len(dataquotes)
-                    print totalquotes
+                    print(totalquotes)
 
                     if re.search(r"!quote random$", message ):
-                        print "** Quote random **"
+                        print("** Quote random **")
 
                         number = random.randint(1,totalquotes)
-                        quote = dbGetOne("SELECT * FROM quotes WHERE number = '"+number+"'")[1]
+                        quote = dbGetOne("SELECT * FROM quotes WHERE number = '"+str(number)+"'")
 
-                        sendMessage(s, str(quote))
+                        sendMessage(s, str(quote[1]))
 
 
                     if re.search(r"!quote [0-9]+$", message ):
-                        print "** Quote <nr> **"
+                        print("** Quote <nr> **")
 
-                        message = message.split(' ')
-                        number = message[1]
+                        messages = message.split(' ')
+                        number = messages[1]
 
-                        quote = dbGetOne("SELECT * FROM quotes WHERE number = '"+number+"'")[1]
-                        sendMessage(s, str(quote))
+                        quote = dbGetOne("SELECT * FROM quotes WHERE number = '"+str(number)+"'")
+                        sendMessage(s, str(quote[1]))
 
 
                     if re.search(r"!delquote [0-9]+", message ) and user in mods:
-                        print "** Remove quote **"
+                        print("** Remove quote **")
 
                         quotenr = message.split(' ', 1)[1]
                         dbExecute('DELETE FROM quotes WHERE number='+str(quotenr)+'')
@@ -231,7 +283,7 @@ while True:
 
 
                     if re.search(r"!addquote", message ) and user in mods:
-                        print "** Add quote **"
+                        print("** Add quote **")
 
                         newquote = str(message.strip().split(' ', 1)[1])
                         date = str(datetime.datetime.now()).split(" ")[0]
@@ -242,6 +294,7 @@ while True:
 
 
         except:
+            print(sys.exc_info()[0])
             pass
         else:
             break
