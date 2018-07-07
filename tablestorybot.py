71d78fe06983018a966ccfcd59c1e06335eca547
Kujio <astroc@gmail.com>
2018-07-07 11:11:05 -0400

    Quote Rewrite:
    Fixes bot crashing when people ask for non-existant quotes.
    Fixes Adding, Deleting Quotes.

--- tablestorybot.py
+++ tablestorybot.py
@@ -440,15 +440,17 @@ while True:
                     if re.search(r"^!quote random$", message ):
                         print("** Quote random **")
 
-                        totalquotes = dbGetOne("SELECT COUNT(quote) FROM quotes")[0]
+                        
 
                         sent = False
                         while sent == False:
                             try:
-                                number = random.randint(1,totalquotes)
-                                quote = dbGetOne("SELECT * FROM quotes WHERE nummer = '"+str(number)+"'")
+                                
+                                quotes = dbGetAll("SELECT * FROM quotes2")
+                                quote = random.choice(quotes)
+                                
 
-                                sendMessage(s, str(quote[1]))
+                                sendMessage(s, "{} #{}".format(quote[1], quote[0]))
                                 sent = True
                             except:
                                 continue
@@ -458,29 +460,39 @@ while True:
                         print("** Quote <nr> **")
 
                         messages = message.split(' ')
-                        number = messages[1]
-
-                        quote = dbGetOne("SELECT * FROM quotes WHERE number = '"+str(number)+"'")
-                        sendMessage(s, str(quote[1]))
-
+                        number = int(messages[1])
+                        print(number)
+                        quote = dbGetOne("SELECT * FROM quotes2 WHERE id = {}".format(int(number)))
+                        print(quote)
+                        if quote is not None:
+                            sendMessage(s, "{} #{}".format(quote[1], quote[0]))
+                        else:
+                            sendMessage(s, "Quote #{} does not exist.".format(number))
 
                     if re.search(r"^!delquote [0-9]+", message ) and (user in mods):
                         print("** Remove quote **")
-
-                        quotenr = message.split(' ', 1)[1]
-                        dbExecute('DELETE FROM quotes WHERE number='+str(quotenr)+'')
-                        sendMessage(s, "Quote #" + quotenr + " deleted.")
+                        messages = message.split(' ')
+                        number = int(messages[1])
+                        quote = dbGetOne("SELECT * FROM quotes2 WHERE id = {}".format(int(number)))
+                        print(quote)
+                        if quote is not None:
+                            dbExecute("DELETE FROM quotes2 WHERE id = {}".format(number))
+                            sendMessage(s, "Quote #{} deleted.".format(number))
+                        else:
+                            sendMessage(s, "Quote #{} does not exist.".format(number))
 
 
                     if re.search(r"^!addquote", message ) and (user in mods):
                         print("** Add quote **")
-
+                        
+                        nextquote  = dbGetOne("SELECT AUTO_INCREMENT FROM INFORMATION_SCHEMA.TABLES WHERE table_name = 'quotes2' AND table_schema = DATABASE( )")[0]
+                        print(nextquote)
                         newquote = str(message.strip().split(' ', 1)[1])
                         date = str(datetime.datetime.now()).split(" ")[0]
-                        totalquotes = str(int(totalquotes+1))
+                        
 
-                        sendMessage(s, "Added quote #" + totalquotes)
-                        dbExecuteargs('INSERT INTO quotes (number, quote) VALUES ( %s, %s)', (totalquotes, "{} {} #{}".format(newquote, date, totalquotes)))
+                        sendMessage(s, "Added quote #{}".format(nextquote))
+                        dbExecuteargs('INSERT INTO quotes2 (quote) VALUES (%s)', ("{} {}".format(newquote, date)))
 
 
         except:
